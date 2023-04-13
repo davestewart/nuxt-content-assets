8a6dc3f25ee1c3261bd32e588451bac38fc1015a
Dave Stewart <dave@davestewart.co.uk>
2023-04-13 12:18:30 +0100

    Tweak sockets and live reload

--- src/runtime/sockets/plugin.ts
+++ src/runtime/sockets/plugin.ts
@@ -15,12 +15,11 @@ export default defineNuxtPlugin(async () => {
         if (src) {
           const isUpdate = event === 'update'
           document
-            .querySelectorAll(`img[src^="${src}"]`)
-            .forEach((el: Element) => {
-              const img = el as HTMLImageElement
-              img.style.opacity = isUpdate ? '1' : '0.2'
+            .querySelectorAll(`:is(img, video, source, embed, iframe):where([src^="${src}"])`)
+            .forEach((el: any) => {
+              el.style.opacity = isUpdate ? '1' : '0.2'
               if (isUpdate) {
-                img.setAttribute('src', `${src}?${new Date().getTime()}`)
+                el.setAttribute('src', `${src}?${new Date().getTime()}`)
               }
             })
         }
