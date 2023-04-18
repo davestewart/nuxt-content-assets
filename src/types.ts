ea75bb9dd00cfd0300edfea75327c0bb7038818b
Dave Stewart <dave@davestewart.co.uk>
2023-04-18 09:29:37 +0100

    Update types

--- src/types.ts
+++ src/types.ts
@@ -10,22 +10,70 @@ export interface SocketInstance {
   addHandler: (handler: Callback) => SocketInstance
 }
 
-export interface ParsedContent {
-  [key: string]: any
+export type ImageSize = Array<'style' | 'src' | 'url' | 'attrs'>
 
-  _path: string
-  _dir: string
-  _draft: boolean
-  _partial: boolean
-  _locale: string
-  _empty: boolean
-  _type: string
+export type AssetConfig = {
+  srcAttr: string
+  content: string[],
+  width?: number
+  height?: number
+}
+
+export interface ParsedContent {
+  /**
+   * The storage id of the file
+   * @example 'content:foo:bar:index.md'
+   */
   _id: string
+
+  /**
+   * The source group identifier
+   * @example 'content'
+   */
   _source: string
+
+  /**
+   * The directory of the file under _source
+   * @example 'foo'
+   */
+  _dir: string
+
+  /**
+   * The route to the file (excluding _source)
+   * @example '/foo/bar'
+   */
+  _path: string
+
+  /**
+   * The file path of the file (excluding _source)
+   * @example 'foo/bar/index.md'
+   */
   _file: string
+
+  /**
+   * The type of the file
+   * @example 'markdown'
+   */
+  _type: string
+
+  /**
+   * The file extension (excluding the dot)
+   * @example 'md'
+   */
   _extension: string
-  title?: string
-  description?: string
-  excerpt?: string
-  body: any
+
+  /**
+   * The AST structure
+   * @example
+   */
+  body: {
+    type: string,
+    children: Array<any>
+  }
+
+  /**
+   * Any other metadata key
+   * @see https://content.nuxtjs.org/guide/writing/markdown/#native-parameters
+   */
+  [key: string | '_draft' | '_partial' | '_locale' | '_empty' | 'title' | 'description' | 'excerpt']: any
 }
