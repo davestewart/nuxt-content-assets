a06679283afe2b2de69ab16ae65f482215f3c4e0
Dave Stewart <dave@davestewart.co.uk>
2023-04-21 19:18:13 +0100

    Fix broken user config (#33)

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,36 @@
-export type Callback = (data: any) => void
+// ---------------------------------------------------------------------------------------------------------------------
+// module
+// ---------------------------------------------------------------------------------------------------------------------
 
-export interface SocketInstance {
-  send: (data: any) => SocketInstance
-  addHandler: (handler: Callback) => SocketInstance
+export interface ModuleOptions {
+  /**
+   * Image size hints
+   *
+   * @example 'attrs style url'
+   * @default 'style'
+   */
+  imageSize?: string | string[] | false
+
+  /**
+   * List of content extensions; anything else as an asset
+   *
+   * @example 'md'
+   * @default 'md csv ya?ml json'
+   */
+  contentExtensions?: string | string[],
+
+  /**
+   * Display debug messages
+   *
+   * @example true
+   * @default false
+   */
+  debug?: boolean
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+// assets
+// ---------------------------------------------------------------------------------------------------------------------
 
 export type ImageSize = Array<'style' | 'src' | 'url' | 'attrs'>
 
@@ -14,13 +41,16 @@ export type AssetConfig = {
   height?: number
 }
 
-
 export interface AssetMessage {
   event: 'update' | 'remove' | 'refresh'
   src?: string
   width?: string
   height?: string
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+// content
+// ---------------------------------------------------------------------------------------------------------------------
 
 export interface ParsedContent {
   /**
@@ -79,4 +109,15 @@ export interface ParsedContent {
    * @see https://content.nuxtjs.org/guide/writing/markdown/#native-parameters
    */
   [key: string | '_draft' | '_partial' | '_locale' | '_empty' | 'title' | 'description' | 'excerpt']: any
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// sockets
+// ---------------------------------------------------------------------------------------------------------------------
+
+export type Callback = (data: any) => void
+
+export interface SocketInstance {
+  send: (data: any) => SocketInstance
+  addHandler: (handler: Callback) => SocketInstance
 }
